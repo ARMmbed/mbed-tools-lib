1fc21e61fcd91b0e5faab13b4004e2679a081a5d
Rosie Miller <rosie.miller@arm.com>
2020-02-06 15:35:59 +0000

    🎨 Sync with template (#8)
    * Keep up to date with changes made to template

--- setup.py
+++ setup.py
@@ -35,7 +35,6 @@ setup(
     keywords="Arm Mbed OS MbedOS Mbed OS",
     include_package_data=True,
     install_requires=[
-        "requests>=2.20",
         "python-dotenv",
     ],
     license="Apache 2.0",
